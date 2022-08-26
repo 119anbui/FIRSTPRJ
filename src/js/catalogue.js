da970245fd81d82fff6bd900b895271d0e2b44c2
davidnguyen31082003@gmail.com <you@example.com>
2022-08-26 11:28:58 +0700

    catalogue update

---
+++ src/js/catalogue.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var splide = new Splide( '.splide', {
+        type   : 'loop',
+        perPage: 3,
+        perMove: 1,
+        breakpoints: {
+            1200: {
+              perPage: 3,
+              gap    : '.7rem',
+            },
+            820: {
+              perPage: 1,
+              gap    : '.7rem',
+             
+            },
+          },
+    });
+    splide.mount();
+    });
